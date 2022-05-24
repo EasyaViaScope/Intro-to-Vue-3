2169ab18513e39668cfcd29f2c91e0faa2a82428
EasyaViaScope <alexandru.ovcinicov@viascope.md>
2022-05-24 16:46:35 +0300

    changes

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks'
+        }
+    }
+})
+
+
+
+// const product = 'Socks'
+
+// const app = Vue.createApp({
+//     data: function()
+// (ES6 shorthand: data() {
+//    
+//} )
+// {
+//         return {
+//             product: 'Socks'
+//         }
+//     }
+// })
